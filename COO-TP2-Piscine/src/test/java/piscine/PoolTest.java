d83a7cdfa33f0d77dd6057d33da45b6538d87971
vabois <loick.vabois@etudiant.univ-lille1.fr>
2017-10-27 09:56:59 +0200

    un bon début (?)

--- COO-TP2-Piscine/src/test/java/piscine/PoolTest.java
+++ COO-TP2-Piscine/src/test/java/piscine/PoolTest.java
@@ -6,6 +6,9 @@
 package piscine;
 
 import static org.junit.Assert.*;
+
+import java.util.NoSuchElementException;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -17,17 +20,50 @@ public class PoolTest {
 	
 	/**
 	 * MockPool pour les tests
-	 * @date 05/10/2017
 	 **/
-	private class RessourcePool implements Ressource {
+	private class MockRessourcePool<R extends Ressource> extends RessourcePool<R> {
+		protected int index;
 		
-		public String description () {return "";}
+		public MockRessourcePool () {
+			super();
+			this.index = index;
+		}
+		
+		@Override
+		public R provideRessource() {
+			if (index > 0) {
+				for (R r : this.pool) {
+					if (r.isGiven() == false) {
+						r.setStatus(true);
+						this.index--;
+						return r;
+					}
+				}
+			} else {
+				throw new NoSuchElementException("No available ressource in " + this.description());
+			}
+			return null;
+		}
+		
+		@Override
+		public void recoverRessource(R ress) {
+			for (R r : this.pool) {
+				if (r==ress) {
+					r.setStatus(false);
+					this.index++;
+					return;
+				}
+			}
+			throw new IllegalArgumentException("Recovered ressource doesn't belong to " + this.description());
+		}
+		
+		public String description () { return "mock pool"; }
 		
 	}
 	
 	@Before
 	public void init () {
-		RessourcePool rp = new RessourcePool();
+		MockRessourcePool rp = new MockRessourcePool();
 	}
 	
 	
